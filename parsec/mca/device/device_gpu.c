cfa4f7754931830615190580a54ff96e7f04be53
Joseph Schuchart <joseph.schuchart@stonybrook.edu>
2026-06-08 14:17:59 -0400

    Remove unused variables
    Spotted in CI. Likely introduced and not removed.
    
    Signed-off-by: Joseph Schuchart <joseph.schuchart@stonybrook.edu>
    (cherry picked from commit ef37d593db19f4a16c4fb694323045ee0cc90d2e)

--- parsec/mca/device/device_gpu.c
+++ parsec/mca/device/device_gpu.c
@@ -2839,7 +2839,7 @@ parsec_device_kernel_scheduler( parsec_device_module_t *module,
 {
     parsec_device_gpu_module_t* gpu_device = (parsec_device_gpu_module_t *)module;
     int rc, exec_stream = 0;
-    parsec_gpu_task_t *progress_task, *out_task_submit = NULL, *out_task_pop = NULL;
+    parsec_gpu_task_t *progress_task = NULL;
     parsec_gpu_task_t *gpu_task = (parsec_gpu_task_t*)_gpu_task;
     int clear_device_counter = PARSEC_DEVICE_CLEAR_EVERY;
 #if defined(PARSEC_DEBUG_NOISIER)
@@ -2975,7 +2975,6 @@ parsec_device_kernel_scheduler( parsec_device_module_t *module,
         goto remove_gpu_task;
     }
     gpu_task = progress_task;
-    out_task_submit = progress_task;
 
  get_data_out_of_device:
     if( NULL != gpu_task ) {  /* This task has completed its execution */
@@ -3006,7 +3005,6 @@ parsec_device_kernel_scheduler( parsec_device_module_t *module,
         goto complete_task;
     }
     gpu_task = progress_task;
-    out_task_pop = progress_task;
 
  fetch_task_from_shared_queue:
     assert( NULL == gpu_task );
